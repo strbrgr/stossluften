55c256491b304e3db5fd84f88aadb027c9e0e6b1
Jochen Stierberger <jochen@stierberger.com>
2024-02-21 16:08:41 -0600

    Updates

--- scd30.py
+++ scd30.py
@@ -1,5 +1,23 @@
 from scd30_i2c import SCD30
 import time
+# import influxdb_client, os, time
+# from influxdb_client import InfluxDBClient, Point, WritePrecision
+# from influxdb_client.client.write_api import SYNCHRONOUS
+#
+# token = os.environ.get("INFLUXDB_TOKEN")
+# org = "jo_enterprise"
+# url = "http://localhost:8086"
+#
+# write_client = influxdb_client.InfluxDBClient(url=url, token=token, org=org)
+#
+# bucket = "co2_bucket"
+#
+# write_api = client.write_api(write_options=SYNCHRONOUS)
+#
+# for value in range(5):
+#     point = Point("measurement1").tag("tagname1", "tagvalue1").field("field1", value)
+#     write_api.write(bucket=bucket, org="jo_enterprise)", record=point)
+#     time.sleep(1)  # separate points by 1 second
 
 scd30 = SCD30()
 
