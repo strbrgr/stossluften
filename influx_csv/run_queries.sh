248cb87ac4354f1eae43a918c64c6ccafc067391
Jochen Stierberger <jochen@stierberger.com>
2024-03-08 10:02:44 -0600

    update query to write from stdout to csv files

--- influx_csv/run_queries.sh
+++ influx_csv/run_queries.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+current_date=$(date +'%Y-%m-%d')
 
-influx query --file=query_office_co2.flux --raw
-influx query --file=query_office_humidity.flux --raw
-influx query --file=query_office_temperature.flux --raw
+influx query --file=query_office_co2.flux --raw > "co2_$current_date}.csv"
+influx query --file=query_office_humidity.flux --raw > "humidity_$current_date}.csv"
+influx query --file=query_office_temperature.flux --raw > "temperature_$current_date}.csv"
+
 
