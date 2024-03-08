eb9f508e91c894cdd12178e694f42cd478f8e107
Jochen Stierberger <jochen@stierberger.com>
2024-03-08 10:25:17 -0600

    fix path and remote /bin

--- influx_csv/run_queries.sh
+++ influx_csv/run_queries.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 current_date=$(date +'%Y-%m-%d')
-export_folder="$(dirname "$0")/export"
 
-influx query --file=query_office_co2.flux --raw > "${export_folder}/co2_${current_date}.csv"
-influx query --file=query_office_humidity.flux --raw > "${export_folder}/humidity_${current_date}.csv"
-influx query --file=query_office_temperature.flux --raw > "${export_folder}/temperature_${current_date}.csv"
+influx query --file=query_office_co2.flux --raw > "export/co2_${current_date}.csv"
+influx query --file=query_office_humidity.flux --raw > "export/humidity_${current_date}.csv"
+influx query --file=query_office_temperature.flux --raw > "export/temperature_${current_date}.csv"
