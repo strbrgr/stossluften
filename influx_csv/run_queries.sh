a059e09c9f168015329b1f55a06977655a243aaf
Jochen Stierberger <jochen@stierberger.com>
2024-03-08 10:02:55 -0600

    remove empty lines

--- influx_csv/run_queries.sh
+++ influx_csv/run_queries.sh
@@ -4,5 +4,3 @@ current_date=$(date +'%Y-%m-%d')
 influx query --file=query_office_co2.flux --raw > "co2_$current_date}.csv"
 influx query --file=query_office_humidity.flux --raw > "humidity_$current_date}.csv"
 influx query --file=query_office_temperature.flux --raw > "temperature_$current_date}.csv"
-
-
