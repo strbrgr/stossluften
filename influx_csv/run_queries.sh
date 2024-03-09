7478bbff48132706acbcda0616a96bdefac0f2d7
Jochen Stierberger <jochen@stierberger.com>
2024-03-08 18:11:29 -0600

    Update the query file to use in cron job

--- influx_csv/run_queries.sh
+++ influx_csv/run_queries.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-current_date=$(date +'%Y-%m-%d')
 
-influx query --file=query_office_co2.flux --raw > "export/co2_${current_date}.csv"
-influx query --file=query_office_humidity.flux --raw > "export/humidity_${current_date}.csv"
-influx query --file=query_office_temperature.flux --raw > "export/temperature_${current_date}.csv"
+docker execute "stossluften-influxdb-1" /bin/bash -c '
+  current_date=$(date +'%Y-%m-%d')
+
+  influx query --file=/influx_csv/query_office_co2.flux --raw > "/influx_csv/export/co2_${current_date}.csv"
+  influx query --file=/influx_csv/query_office_humidity.flux --raw > "/influx_csv/export/humidity_${current_date}.csv"
+  influx query --file=/influx_csv/query_office_temperature.flux --raw > "/influx_csv/export/temperature_${current_date}.csv"
+'
